ef6c2febeda55cd1ad1fe07a3b1651cce82339be
MochiChoco <87297588+MochiChoco@users.noreply.github.com>
2022-03-08 19:48:46 +0900

    fix macos

--- install/macos.sh
+++ install/macos.sh
@@ -4,14 +4,14 @@ set -eu
 
 
 # XCode Command Line Tools
-if [ -d "/Library/Developer/CommandLineTools" ] || [ -d "/Applications/Xcode.app/Contents/Developer" ]; then
+if [ ! -d "/Library/Developer/CommandLineTools" ] || [ ! -d "/Applications/Xcode.app/Contents/Developer" ]; then
     printf "$PRINTF_CYAN $PRINTF_DELETE_LINE" "Installing XCode Command Line Tools"
     xcode-select --install
 fi
 
 
 # Homebrew
-if type brew > /dev/null 2>&1; then
+if !( type brew > /dev/null 2>&1 ); then
     printf "$PRINTF_CYAN $PRINTF_DELETE_LINE" "Installing Homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
