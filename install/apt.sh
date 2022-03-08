5fd91f04f3b2115d7fddc2a7ff977e19b838ef1e
MochiChoco <87297588+MochiChoco@users.noreply.github.com>
2022-03-08 13:26:28 +0900

    fix apt-fast

--- install/apt.sh
+++ install/apt.sh
@@ -54,6 +54,6 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
 sudo apt update
-sudo apt install -y apt-fast
+DEBIAN_FRONTEND=noninteractive sudo apt install -y apt-fast
 sudo apt-fast install -y --install-recommends winehq-staging
 sudo apt-fast install -y $PACKAGE_LIST
