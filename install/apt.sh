9836dad48f65c2ea5977e4125ac7891faf0cca2e
MochiChoco <87297588+MochiChoco@users.noreply.github.com>
2022-03-08 16:35:03 +0900

    test winehq-staging

--- install/apt.sh
+++ install/apt.sh
@@ -60,6 +60,7 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
 sudo apt update
+sudo apt upgrade -y
 
 ## apt-fast
 printf "$PRINTF_CYAN $PRINTF_DELETE_LINE" "Installing apt-fast..."
@@ -81,7 +82,7 @@ APTCACHE='/var/cache/apt/archives'
 
 ## winehq
 printf "$PRINTF_CYAN $PRINTF_DELETE_LINE" "Installing winehq-staging..."
-sudo apt-fast install -y winehq-staging
+sudo apt-fast install -y --install-recommends winehq-staging
 
 printf "$PRINTF_CYAN $PRINTF_DELETE_LINE" "Installing packages..."
 sudo apt-fast install -y $PACKAGE_LIST
