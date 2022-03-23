790ca3180ad82394bc5f9ff76288951e051b3112
MochiChoco <87297588+MochiChoco@users.noreply.github.com>
2022-03-23 22:48:38 +0900

    Fix: without apt-fast

--- install/apt.sh
+++ install/apt.sh
@@ -68,7 +68,7 @@ fi
 
 # apt install
 ## gh
-sudo apt-fast install -y curl
+sudo apt install -y curl
 printf "$PRINTF_CYAN $PRINTF_DELETE_LINE" "Setting gh..."
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
