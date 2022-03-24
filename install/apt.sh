952a9ed5479b3f61ebda7eae39da4ab0a9d1220d
MochiChoco <87297588+MochiChoco@users.noreply.github.com>
2022-03-24 13:24:50 +0900

    Test3: apt-fast

--- install/apt.sh
+++ install/apt.sh
@@ -35,11 +35,11 @@ export PACKAGE_LIST=$(echo "$PACKAGE_LIST" | tr "\n" " ")
 if [ "$PLATFORM" = "Debian" ]; then
     sudo apt install -y curl gnupg2 lsb-release wget
 
-    # ### apt-fast
-    # printf "$PRINTF_CYAN $PRINTF_DELETE_LINE" "Setting apt-fast..."
-    # echo "deb http://ppa.launchpad.net/apt-fast/stable/ubuntu $(curl -fsSL 'https://github.com/tianon/docker-brew-ubuntu-core/raw/dist-amd64/latest') main" | sudo tee /etc/apt/sources.list.d/apt-fast.list > /dev/null
-    # echo "deb-src http://ppa.launchpad.net/apt-fast/stable/ubuntu $(curl -fsSL 'https://github.com/tianon/docker-brew-ubuntu-core/raw/dist-amd64/latest') main" | sudo tee -a /etc/apt/sources.list.d/apt-fast.list > /dev/null
-    # sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A2166B8DE8BDC3367D1901C11EE2FF37CA8DA16B
+    ### apt-fast
+    printf "$PRINTF_CYAN $PRINTF_DELETE_LINE" "Setting apt-fast..."
+    echo "deb http://ppa.launchpad.net/apt-fast/stable/ubuntu $(curl -fsSL 'https://github.com/tianon/docker-brew-ubuntu-core/raw/dist-amd64/latest') main" | sudo tee /etc/apt/sources.list.d/apt-fast.list > /dev/null
+    echo "deb-src http://ppa.launchpad.net/apt-fast/stable/ubuntu $(curl -fsSL 'https://github.com/tianon/docker-brew-ubuntu-core/raw/dist-amd64/latest') main" | sudo tee -a /etc/apt/sources.list.d/apt-fast.list > /dev/null
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A2166B8DE8BDC3367D1901C11EE2FF37CA8DA16B
 
     ## winehq
     printf "$PRINTF_CYAN $PRINTF_DELETE_LINE" "Setting winehq..."
@@ -53,9 +53,9 @@ if [ "$PLATFORM" = "Debian" ]; then
 elif [ "$PLATFORM" = "Ubuntu" ]; then
     sudo apt install -y lsb-release software-properties-common wget
 
-    # ### apt-fast
-    # printf "$PRINTF_CYAN $PRINTF_DELETE_LINE" "Setting apt-fast..."
-    # sudo add-apt-repository ppa:apt-fast/stable
+    ### apt-fast
+    printf "$PRINTF_CYAN $PRINTF_DELETE_LINE" "Setting apt-fast..."
+    sudo add-apt-repository ppa:apt-fast/stable
 
     ### winehq
     printf "$PRINTF_CYAN $PRINTF_DELETE_LINE" "Setting winehq..."
