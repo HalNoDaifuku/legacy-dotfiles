fa7fbf95013c037ef788d6e0571ce8430ac6667e
MochiChoco <87297588+MochiChoco@users.noreply.github.com>
2022-03-24 15:19:49 +0900

    Update: Check command alias.zsh

--- .settings/zsh/alias.zsh
+++ .settings/zsh/alias.zsh
@@ -2,14 +2,21 @@
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
-alias diff='colordiff'
 alias tree='tree -C'
 alias ls='ls -F --color=auto'
 alias ll='ls -lh'
 alias la='ls -a'
-alias exa='exa -F --color=auto'
-alias exal='exa -lh'
-alias exaa='exa -a'
+
+if type colordiff > /dev/null 2>&1; then
+    alias diff='colordiff'
+fi
+
+if type exa > /dev/null 2>&1; then
+    alias exa='exa -F --color=auto'
+    alias exal='exa -lh'
+    alias exaa='exa -a'
+fi
+
 # alias ls='exa'
 # alias cat='bat'
 # alias find='fd'
